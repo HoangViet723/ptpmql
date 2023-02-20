0335bcda153915163ce9e37e23bd952f7a9bfd46
HoangViet723 <hoangvietn313@gmail.com>
2023-02-20 16:04:26 +0700

    first commit

---
+++ BaiThucHanhTest/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaiThucHanhTest.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
