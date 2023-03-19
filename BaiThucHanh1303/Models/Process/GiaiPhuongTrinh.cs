8eba5a43765c98f743695a36443e625a7c2ba94e
HoangViet723 <hoangvietn313@gmail.com>
2023-03-19 22:19:20 +0700

    commit note

---
+++ BaiThucHanh1303/Models/Process/GiaiPhuongTrinh.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiThucHanh1303.Models.Process
+{
+    public class GiaiPhuongTrinh
+    {
+        public string GiaiPhuongTrinhB1(double a, double b)
+        {
+            double x = -b / a;
+            return "Nghiem cua phuong trinh x la: " + x;
+        }
+    }
+}
+
