436d6cda1d5ac3f40b51a6baa0488cb3845706e6
HoangViet723 <hoangvietn313@gmail.com>
2023-03-19 22:17:04 +0700

    del

--- BaiThucHanh1303/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace baithuchanh1303.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
