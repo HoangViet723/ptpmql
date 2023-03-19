ca1821cbe2717e1803a3167ce301753436f7754f
HoangViet723 <hoangvietn313@gmail.com>
2023-03-19 22:22:44 +0700

    first commit

---
+++ BaiToanTamGiac/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaiToanTamGiac.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ BaiToanTamGiac/Models/Process/TamGiac.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiToanTamGiac.Models.Process
+{
+    public class TamGiac
+    {
+        public string BaiToanTamGiac(int a, int b, int c)
+        {
+            return "";
+        }
+    }
+}
